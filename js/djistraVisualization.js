8d8615df9691871c50c94e0e46959a712077ce83
Nil Patel <nilpatel.cs@gmail.com>
2020-11-28 16:01:02 -0500

    ios animation fix

--- js/djistraVisualization.js
+++ js/djistraVisualization.js
@@ -1,97 +1,134 @@
-const canvas = document.getElementById("djikstra");
-const context = canvas.getContext("2d");
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
-var script = document.createElement('script');
-script.src = "//code.jquery.com/jquery-3.5.1.slim.min.js";
-script.type = 'text/javascript';
+const canvas = document.getElementById('djikstra');
+const context = canvas.getContext('2d');
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+var particleArray = null;
+var posX, posY;
+var mobile =null;
+if(window.innerWidth<=1024)
+    mobile = true;
+else{
+    mobile = false;
 
-if (document.body.clientWidth > 1024) {
-    var particleArray = null;
-    let mouse = {
-        x: -1,
-        y: null
-    }
+}
+var mouse = {
+    x: -9,
+    y: null
+}
 
-
-    window.addEventListener("mousemove", function (event) {
-		spawn();
-
-    });
-
-    window.onresize = function () { 
-        if(document.body.clientWidth<=1024){
+ window.onresize = function () {
+        if(mobile && window.innerWidth>1024){
+            mobile = false;
             location.reload();
         }
-        else{
-        canvas.width = document.body.clientWidth;
-        canvas.height = document.body.clientHeight;
-        } 
-    } //if  I dont do this resize ruins the particles
+        else if(!mobile && window.innerWidth<=1024){
+            mobile = true;
+            location.reload();
+        }
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        if(mobile)
+            spawn();
+    }
 
-    window.addEventListener("mouseout", function (event) {
-        context.clearRect(0, 0, innerWidth, innerHeight);
-        mouse.x = -5;
-        particleArray = null;
+/*event for mouse coordinates and leaving entering window for desktop */
+    if(!mobile){
 
+        window.addEventListener("mousemove", function showCoords(event) {
+            posX = event.clientX;
+            posY = event.clientY;
+            spawn();
+        });
+
+        window.addEventListener("mouseout", function (event) {
+            context.clearRect(0, 0, innerWidth, innerHeight);
+            mouse.x = -10;
+            particleArray = null;
     });
+    }
 
-	function spawn() {
-		context.clearRect(0, 0, innerWidth, innerHeight);
-		dx = event.x - mouse.x;
-		dy = event.y - mouse.y;
-		mouse.x = event.x;
-		mouse.y = event.y;
-		if (particleArray == null) {
-			particleArray = [];
-			for (i = 0; i < 30; i++) {
-				particleArray[i] = new particle(i);
-			}
-		}
-		else {
-			for (i = 0; i < particleArray.length; i++) {
-				particleArray[i].follow(dx, dy);
-			}
-		}
-	}
+    function spawn() {
+        context.clearRect(0, 0, innerWidth, innerHeight);
+        if(mobile){
+                particleArray = [];
+                for (i = 0; i < 50; i++) {
+                    particleArray[i] = new particle(i);
+                }
+        }
+        else{
+            dx = posX - mouse.x;
+		    dy = posY - mouse.y;
+		    mouse.x = posX;
+		    mouse.y = posY;
+		    if (particleArray == null||mouse.x<0) {
+			    particleArray = [];
+			    for (i = 0; i < 30; i++) {
+				    particleArray[i] = new particle(i);
+			    }
+		    }
+		    else {
+			    for (i = 0; i < particleArray.length; i++) {
+                    particleArray[i].follow(dx, dy);
+
+			    }
+		    }
+	    }
+    }
 
 
     class particle {
 
         constructor(i) {
             this.id = i;
-            this.x = mouse.x + plusMinus() * Math.random() * canvas.height / 10;
-            this.y = mouse.y + plusMinus() * Math.random() * Math.sqrt(Math.pow(canvas.height / 10, 2) - Math.pow(this.x - mouse.x, 2));
-            this.size = 2;
-			this.xVelocity = plusMinus() * Math.random() * (canvas.height / 5000);
-            this.yVelocity = plusMinus() * Math.random() * (canvas.height / 5000);
-            context.beginPath();
-            context.fillStyle = "rgba(240,240,240,.8)";
-            context.strokeStyle = "rgba(240,240,240,.8)";
-            context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-            context.closePath();
-            context.fill();
+
+            if(mobile){
+                this.x = Math.random() * canvas.width / 4 * plusMinus() + canvas.width / 2;
+                this.y = canvas.height / 2 + plusMinus() *Math.random()* Math.sqrt(Math.pow(canvas.width / 4, 2) - Math.pow(this.x - canvas.width / 2, 2));
+                if(canvas.width<650)
+                    this.size = 1;
+                else
+                    this.size = 1.5;
+                this.xVelocity = plusMinus() * Math.random() * (canvas.width / 1200);
+                this.yVelocity = plusMinus() * Math.random() * (canvas.width / 1200);
+            }
+            else{
+                this.x = mouse.x + plusMinus() * Math.random() * canvas.height / 10;
+                this.y = mouse.y + plusMinus() * Math.random() * Math.sqrt(Math.pow(canvas.height / 10, 2) - Math.pow(this.x - mouse.x, 2));
+                this.size = 2;
+                this.xVelocity = plusMinus() * Math.random() * (canvas.height / 5000);
+                this.yVelocity = plusMinus() * Math.random() * (canvas.height / 5000);
+            }
+            this.draw();
         }
-        follow(x, y) {
-            this.x += x;
-            this.y += y;
-            context.beginPath();
-            context.fillStyle = "rgba(240,240,240,.8)";
-            context.strokeStyle = "rgba(240,240,240,.8)";
-            context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-            context.closePath();
-            context.fill();
-        }
+
         draw() {
             context.beginPath();
-            context.fillStyle = "rgba(240,240,240,.8)";
-            context.strokeStyle = "rgba(240,240,240,.8)";
+            if(mobile){
+                if(canvas.width<650){
+                    context.fillStyle = "rgba(255,255,255,.5)";
+                    context.strokeStyle = "rgba(255,255,255,.5)";
+                }
+                else{
+                    context.fillStyle = "rgba(255,255,255,.8)";
+                    context.strokeStyle = "rgba(255,255,255,.8)";
+                }
+            }
+            else{
+                context.fillStyle = "rgba(240,240,240,.8)";
+                context.strokeStyle = "rgba(240,240,240,.8)";
+            }
             context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
             context.closePath();
             context.fill();
         }
         move() {
-            if (canvas.height / 10 < Math.sqrt(Math.pow(this.x + this.xVelocity - mouse.x, 2) + Math.pow(this.y + this.yVelocity - mouse.y, 2))) {
+            if(mobile){
+                if (canvas.width/4<Math.sqrt(Math.pow(this.x + this.xVelocity - canvas.width / 2, 2) + Math.pow(this.y + this.yVelocity - canvas.height / 2, 2))) {
+                    this.xVelocity *= -1;
+                    this.yVelocity *= -1;
+                }
+            }
+            else if (canvas.height / 10 < Math.sqrt(Math.pow(this.x + this.xVelocity - mouse.x, 2) + Math.pow(this.y + this.yVelocity - mouse.y, 2))) {
                 this.xVelocity *= -1;
                 this.yVelocity *= -1;
             }
@@ -99,165 +136,95 @@ if (document.body.clientWidth > 1024) {
             this.y += this.yVelocity;
             this.draw();
         }
+
+        follow(x, y) {
+            this.x += x;
+            this.y += y;
+            this.draw();
+        }
     }
+
     function drawLine(u, v) {
-        context.beginPath();
-        context.setLineDash([]);
-        context.strokeStyle = "rgba(255,140,0,.8)";
-        context.lineWidth = 15/dist(u, v);
-        context.moveTo(u.x, u.y);
-        context.lineTo(v.x, v.y);
-        context.stroke();
+        if(mobile){
+          context.beginPath();
+          context.setLineDash([]);
+          context.strokeStyle = "rgba(255,140,0,.8)";
+          if(canvas.width<650)
+                context.lineWidth = 10/dist(u, v);
+          else
+            context.lineWidth = 15/dist(u, v);
+          context.moveTo(u.x, u.y);
+          context.lineTo(v.x, v.y);
+          context.stroke();
+        }
+        else{
+            context.beginPath();
+            context.setLineDash([]);
+            context.strokeStyle = "rgba(255,140,0,.8)";
+            context.lineWidth = 15/dist(u, v);
+            context.moveTo(u.x, u.y);
+            context.lineTo(v.x, v.y);
+            context.stroke();
+            }
     }
-    
+
     function drawLineFormer(u, v) {
-        context.beginPath();
-        context.strokeStyle = "rgba(200,200,200,.5)";
-        context.moveTo(u.x, u.y);
-        context.lineTo(v.x, v.y);
-        context.lineWidth = 10/dist(u, v);
-        context.setLineDash([5]);
-        context.stroke();
+        if(mobile){
+            context.beginPath();
+            context.strokeStyle = "rgba(255,255,255,.5)";
+            context.moveTo(u.x, u.y);
+            context.lineTo(v.x, v.y);
+            if(canvas.width<650)
+                context.lineWidth = 5/dist(u, v);
+            else
+                context.lineWidth = 10/dist(u, v);
+            context.setLineDash([5]);
+            context.stroke();
+        }
+        else{
+            context.beginPath();
+            context.strokeStyle = "rgba(200,200,200,.5)";
+            context.moveTo(u.x, u.y);
+            context.lineTo(v.x, v.y);
+            context.lineWidth = 10/dist(u, v);
+            context.setLineDash([5]);
+            context.stroke();
+        }
     }
+
 
     function animate() {
         requestAnimationFrame(animate);
-        context.clearRect(0, 0, innerWidth, innerHeight);
-        if (mouse.x < 0) {
-            return;
-        }
-        else {
+        if(mobile||mouse.x>0){
+            context.clearRect(0, 0, innerWidth, innerHeight);
             for (i = 0; i < particleArray.length; i++) {
                 particleArray[i].move();
-
             }
             djikstra();
         }
-    }
-    animate();
-}
-else{
-   
-var particleArray = null;
-
-
-function spawn() {
-	context.clearRect(0, 0, innerWidth, innerHeight);
-
-		particleArray = [];
-		for (i = 0; i < 50; i++) {
-            particleArray[i] = new particle(i);
-    }
-
-	
-}
-window.onresize = function () {
-    if(document.body.clientWidth>1024){
-        location.reload();
-    }
-    else{
-    canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-    spawn();
-    }
-}
-class particle {
-
-    constructor(i) {
-      
-        this.id = i;
-        this.x = Math.random() * canvas.width / 4 * plusMinus() + canvas.width / 2;
-
-        this.y = canvas.height / 2 + plusMinus() *Math.random()* Math.sqrt(Math.pow(canvas.width / 4, 2) - Math.pow(this.x - canvas.width / 2, 2));
-        if(canvas.width<650)
-            this.size = 1;
-        else
-            this.size = 1.5;
-        this.xVelocity = plusMinus() * Math.random() * (canvas.width / 1200);
-        this.yVelocity = plusMinus() * Math.random() * (canvas.width / 1200);
-        this.draw();
-    }
-
-    draw() {
-        context.beginPath();
-        if(canvas.width<650){
-            context.fillStyle = "rgba(255,255,255,.5)";
-            context.strokeStyle = "rgba(255,255,255,.5)";
-        }
         else{
-            context.fillStyle = "rgba(255,255,255,.8)";
-            context.strokeStyle = "rgba(255,255,255,.8)";
-        }
-        context.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        context.closePath();
-        context.fill();
+            return;
+            }
     }
-    move() {
-        if (canvas.width/4<Math.sqrt(Math.pow(this.x + this.xVelocity - canvas.width / 2, 2) + Math.pow(this.y + this.yVelocity - canvas.height / 2, 2))) {
-            this.xVelocity *= -1;
-            this.yVelocity *= -1;
-        }
-                
-         
-    
-        this.x += this.xVelocity;
-        this.y += this.yVelocity;
-        this.draw();
-    }
+    if(mobile)
+        spawn();
+
+/*shared functions */
+function dist(u, v) {
+    result = Math.sqrt(Math.pow(u.x - v.x, 2) + Math.pow(u.y - v.y, 2));
+    return result;
 }
-
-function drawLine(u, v) {
-    context.beginPath();
-    context.setLineDash([]);
-    context.strokeStyle = "rgba(255,140,0,.8)";
-    if(canvas.width<650)
-            context.lineWidth = 10/dist(u, v);
-    else
-        context.lineWidth = 15/dist(u, v);
-    context.moveTo(u.x, u.y);
-    context.lineTo(v.x, v.y);
-    context.stroke();
-}
-
-function drawLineFormer(u, v) {
-    context.beginPath();
-    context.strokeStyle = "rgba(255,255,255,.5)";
-    context.moveTo(u.x, u.y);
-    context.lineTo(v.x, v.y);
-    if(canvas.width<650)
-        context.lineWidth = 5/dist(u, v);
-    else
-        context.lineWidth = 10/dist(u, v);
-    context.setLineDash([5]);
-    context.stroke();
-}
-
-
-function animate() {
-    requestAnimationFrame(animate);
-    context.clearRect(0, 0, innerWidth, innerHeight);
-        for (i = 0; i < particleArray.length; i++) {
-            particleArray[i].move();
-    } 
-    djikstra();
-   
-    
-}
-spawn();
-animate();
-
-}
-
 function plusMinus() {
-	let sign = Math.random() - .5;
-	if (sign < 0) {
-		sign = -1;
-	}
-	else {
-		sign = 1;
-	}
-	return sign;
+    let sign = Math.random() - .5;
+    if (sign < 0) {
+        sign = -1;
+    }
+    else {
+        sign = 1;
+    }
+    return sign;
 }
+
 function djikstra() {
     var connected = [];
     var distance = [];
@@ -291,7 +258,8 @@ function djikstra() {
     for (i = 1; i < particleArray.length; i++) {
         drawLine(particleArray[previous[i]], particleArray[i]);
     }
-  
+    //calculate the distance between two particles
+
 
 
     //return index of smallest value in array
@@ -305,10 +273,4 @@ function djikstra() {
         return lowest;
     }
 }
-
-
-  //calculate the distance between two particles
-  function dist(u, v) {
-    result = Math.sqrt(Math.pow(u.x - v.x, 2) + Math.pow(u.y - v.y, 2));
-    return result;
-}
+animate();
